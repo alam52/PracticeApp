e8d7bc3e4821d07c5fcc477181cbfbb7072b68a4
alam52 <zalam307@gmail.com>
2022-04-27 00:03:58 +0530

    Added redirect and path converters

--- challenges/views.py
+++ challenges/views.py
@@ -1,20 +1,40 @@
 from django.forms import model_to_dict
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+monthly_msgs = {
+    'january':'First month',
+    'february': 'Second month',
+    'march': 'Third month',
+    'april': 'Fourth month',
+    'may': 'Fifth month',
+    'june': 'Sixth month',
+    'july': 'Seventh month',
+    'august': 'Eight month',
+    'september': 'Ninth month',
+    'october': 'Tenth month',
+    'november': 'Eleventh month',
+    'december': 'Twelfth month'
+    }
 
 # Create your views here.
 def index(request):
     return HttpResponse("Index Page")
 
+def monthy_challenges_num(request, month):
+    months = list(monthly_msgs.keys())
+    
+    try:
+        msg_by_num = months[month-1]
+    except:
+        return HttpResponseNotFound('Incorrect Month Number')
+    return HttpResponseRedirect('/challenges/' + msg_by_num)
+
 def monthly_challenges(request, month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text = 'January'
-    elif month == 'february':
-        challenge_text = 'February'
-    elif month == 'march':
-        challenge_text = 'March'
-    else:
-        return HttpResponseNotFound("Incorrect month in URL")
+
+    try:
+        challenge_text = monthly_msgs[month]
+    except:
+        return HttpResponseNotFound('Incorrect month in URL')
     return HttpResponse(challenge_text)
